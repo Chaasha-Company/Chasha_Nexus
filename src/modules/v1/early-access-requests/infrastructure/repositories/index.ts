8ef23df54ccb3fc18741122b814615ef0d191f86
Erfan Abouei <erfangamer138787@gmail.com>
2026-08-25 03:33:49 +0330

    refactor(early-access-requests): correct repository filename typo

--- src/modules/v1/early-access-requests/infrastructure/repositories/index.ts
+++ src/modules/v1/early-access-requests/infrastructure/repositories/index.ts
@@ -1,4 +1,4 @@
-export * from './create-early-access-request.repositroy';
+export * from './create-early-access-request.repository';
 export * from './early-access-request-status';
 export * from './find-early-access-request-by-phone-number.repository';
 export * from './find-all-early-access-request.repository';
